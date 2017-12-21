dffe046159188327bf521f02b2d1fd2ed05ef2a8
Jason Vance <jbvance@gmail.com>
2017-12-21 03:35:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { Garth: 'The Dance' };
 
 var updatePlaylist = (playlist, artistName, songTitle) => {
-  return Object.assign({}, playlist, { [artistName]: songTitle });
+  return Object.assign(playlist, { [artistName]: songTitle });
 };
